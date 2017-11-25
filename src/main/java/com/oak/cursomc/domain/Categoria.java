0bbd8d46790bd4cb47afc13751e13827e6587854
Anderson Carvalho Martins <anderson.cvlh@gmail.com>
2017-11-25 10:29:03 -0200

    Mapeamento da entidade categoria

--- src/main/java/com/oak/cursomc/domain/Categoria.java
+++ src/main/java/com/oak/cursomc/domain/Categoria.java
@@ -2,12 +2,21 @@ package com.oak.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 4026069546511154965L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
+	
 	public Long getId() {
 		return id;
 	}
